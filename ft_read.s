37b6e2315fb161fa09edb44ec21ebb426572d25c
Max BRIGNOLI <mbrignol@e1r12p1.42.fr>
2020-01-23 17:22:51 +0100

    mandatory

--- ft_read.s
+++ ft_read.s
@@ -2,6 +2,6 @@ section .text
     global _ft_read
 
 _ft_read:
-    mov rax, 0
+    mov rax,  0x2000003
     syscall
     ret
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int ft_strcmp(char *s1, char *s2);
 int ft_write(int fd, char *str, int size);
 char *ft_strcpy(char *dest, char *src);
-//int ft_read(int fd, void *buffer, unsigned int count);
+int ft_read(int fd, void *buffer, unsigned int count);
 int ft_strlen(char *str);
 char *ft_strdup(char *str);
 
@@ -23,16 +23,20 @@ int main(void)
 {
     char *str;
     char *dest;
+    char *src;
+    char *string;
+    char *read;
 
-//    dest = ft_strnew(9, 'A');
-//	str = ft_strnew(8, 'Z');
+    dest = ft_strnew(9, 'A');
+	src = ft_strnew(8, 'Z');
+	string = "COUCOU";
 	str = "YOOOOOOOO";
 
-//	ft_write(1, "YO\n", 3);
-//	printf("%s\n", ft_strdup(str));
-	printf ("%s\n", ft_strdup(str));
-//	ft_strdup(str);
-//	printf("%d\n", ft_strcmp(dest, str));
-//	printf("%s\n", ft_strcpy(dest, str));
-//	printf("%d\n", ft_strlen(str));
+	ft_read(0, read, 5);
+	///printf("%s\n", read);
+	ft_write(1, "YO\n", 3);
+	printf("%s\n", ft_strdup(str));
+	printf("%d\n", ft_strcmp(string, str));
+	printf("%s\n", ft_strcpy(dest, src));
+	printf("%d\n", ft_strlen(str));
 }
